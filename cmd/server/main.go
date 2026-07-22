0b98107908b0737e35cece9c02cd2f69b5683e0d
Yash Jain <yj99901@gmail.com>
2026-07-22 16:14:36 +0000

    Initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	// RabbitMQ connection string
+	connString := "amqp://guest:guest@localhost:5672/"
+
+	// Connect to RabbitMQ
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		fmt.Printf("Failed to connect to RabbitMQ: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ch.Close()
+
+	fmt.Println("Successfully connected to RabbitMQ!")
+
+	err = pubsub.PublishJSON(
+		ch,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Wait for Ctrl+C
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Shutting down...")
+}
---
+++ internal/pubsub/pubsub.go
@@ -0,0 +1,27 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	body, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+
+	return ch.PublishWithContext(
+		context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        body,
+		},
+	)
+}
