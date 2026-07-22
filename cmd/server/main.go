e30ec3ec983a72def0df10aecac68f0896f88026
Yash Jain <yj99901@gmail.com>
2026-07-22 16:45:54 +0000

    Add interactive pause/resume/quit REPL loop to server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,50 +3,66 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	// RabbitMQ connection string
-	connString := "amqp://guest:guest@localhost:5672/"
+	fmt.Println("Starting Peril server...")
 
-	// Connect to RabbitMQ
+	const connString = "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(connString)
 	if err != nil {
-		fmt.Printf("Failed to connect to RabbitMQ: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
 	}
 	defer conn.Close()
+	fmt.Println("Successfully connected to RabbitMQ!")
 
 	ch, err := conn.Channel()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("could not create channel: %v", err)
 	}
 	defer ch.Close()
 
-	
-	err = pubsub.PublishJSON(
-		ch,
-		routing.ExchangePerilDirect,
-		routing.PauseKey,
-		routing.PlayingState{
-			IsPaused: true,
-		},
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Successfully connected to RabbitMQ!")
-	
-	// Wait for Ctrl+C
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+	gamelogic.PrintServerHelp()
 
-	fmt.Println("Shutting down...")
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		switch words[0] {
+		case "pause":
+			fmt.Println("Sending pause message...")
+			err = pubsub.PublishJSON(
+				ch,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{IsPaused: true},
+			)
+			if err != nil {
+				log.Printf("could not publish pause message: %v", err)
+			}
+		case "resume":
+			fmt.Println("Sending resume message...")
+			err = pubsub.PublishJSON(
+				ch,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{IsPaused: false},
+			)
+			if err != nil {
+				log.Printf("could not publish resume message: %v", err)
+			}
+		case "quit":
+			fmt.Println("Exiting...")
+			return
+		default:
+			fmt.Println("I don't understand that command.")
+		}
+	}
 }
